1df067f3320692fb1811af1bb1333d5e78671fc9
Tommy Myers <tommymyers1101@gmail.com>
2021-02-26 01:06:46 +0000

    Get versions from GitHub API releases endpoint

--- tmod.js
+++ tmod.js
@@ -2,18 +2,38 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/versions', ((req, res) => {
-    const versions = {
-        latest: '1.0.1',
-        versions: [
-            {
-                id: "1.0.1",
-                url: "https://google.com"
+const request = require('request');
+
+app.get('/versions', (req, res) => {
+    request('https://api.github.com/repos/tommymyers/tmod/releases',
+        {
+            json: true,
+            headers: {
+                'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+                'Accept': 'application/vnd.github.v3+json'
             }
-        ]
-    }
-    res.send(versions)
-}))
+        }, (err, requestResponse, body) => {
+
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+
+        if (body.length > 0) {
+            const latestVersion = body[0]
+            res.send({
+                latest: {
+                    id: latestVersion.tag_name,
+                    url: latestVersion.html_url
+                }
+            })
+        } else {
+            res.statusCode = 204
+            res.send('no versions found')
+        }
+
+    });
+})
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
